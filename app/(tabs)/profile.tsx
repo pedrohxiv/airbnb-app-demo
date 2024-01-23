50f44f7aac2b5f7cf30652c87da565bf9e393048
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-01-23 16:35:50 -0300

    Clerk auth with google, apple and facebook created

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { useAuth } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 const ProfilePage = () => {
+  const { signOut, isSignedIn } = useAuth();
+
   return (
     <View>
-      <Text>ProfilePage</Text>
+      <Button title="Logout" onPress={() => signOut()} />
+      {!isSignedIn && (
+        <Link href="/(modals)/login">
+          <Text>Login</Text>
+        </Link>
+      )}
     </View>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
 import { Stack, useRouter } from "expo-router";
+import * as SecureStore from "expo-secure-store";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { TouchableOpacity } from "react-native";
@@ -20,6 +22,23 @@ const RootLayout = () => {
     "mon-b": require("../assets/fonts/Montserrat-Bold.ttf"),
   });
 
+  const tokenCache = {
+    async getToken(key: string) {
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch (error) {
+        return null;
+      }
+    },
+    async saveToken(key: string, value: string) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (error) {
+        return;
+      }
+    },
+  };
+
   useEffect(() => {
     if (error) throw error;
   }, [error]);
@@ -34,11 +53,26 @@ const RootLayout = () => {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <RootLayoutNav />
+    </ClerkProvider>
+  );
 };
 
 const RootLayoutNav = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+
   const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push("/(modals)/login");
+    }
+  }, [isLoaded]);
 
   return (
     <Stack>
